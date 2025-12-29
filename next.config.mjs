1d54ef0f8c5437e0435c754da3649fb9905bed79
weilaiyuan <weilaiyuan86@gmail.com>
2025-12-29 18:52:36 +0800

    final fix: skip all checks

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true, // 强制忽略 TypeScript 错误
-  },
-  eslint: {
-    ignoreDuringBuilds: true, // 强制忽略 ESLint 检查
-  },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
 };
 export default nextConfig;
