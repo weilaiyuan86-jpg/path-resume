1aab4218dd73c5aa781e0ae52538c925abf96636
weilaiyuan <weilaiyuan86@gmail.com>
2025-12-29 20:21:43 +0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,13 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // 确保包含 app 目录
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        brand: {
-          blue: '#2563eb',
-          orange: '#f97316',
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
+export default config;
