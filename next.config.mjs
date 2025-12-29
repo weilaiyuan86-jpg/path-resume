571feccf73eb27407117ee88bee40d5f2c6cbfb6
weilaiyuan <weilaiyuan86@gmail.com>
2025-12-29 19:05:34 +0800

    fix: remove unknown build option

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
+  typescript: {
+    ignoreBuildErrors: true, // 核心：跳过类型检查
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // 核心：构建时忽略 ESLint
+  },
 };
 export default nextConfig;
